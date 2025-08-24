42b639993b6fa35ce221d2a2bf975f870836851d
philip82148 <92205270+philip82148@users.noreply.github.com>
2025-08-25 08:55:46 +0900

    ESLintのルールを強化

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,11 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import sortExports from "eslint-plugin-sort-exports";
+import unusedImports from "eslint-plugin-unused-imports";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +16,8 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
+  js.configs.recommended,
+  eslintConfigPrettier,
   {
     ignores: [
       "node_modules/**",
@@ -19,6 +26,43 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    plugins: {
+      "unused-imports": unusedImports,
+      "sort-exports": sortExports,
+    },
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+          },
+        },
+      ],
+      "import/newline-after-import": "error",
+      "unused-imports/no-unused-imports": "error",
+      "sort-exports/sort-exports": [
+        "error",
+        { sortDir: "asc", sortExportKindFirst: "type" },
+      ],
+      "no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          caughtErrors: "all",
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+    },
   },
 ];
 
